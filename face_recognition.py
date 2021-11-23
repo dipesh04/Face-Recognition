37a2bf401dad8d2c44463e342f70cb261232d23a
Dipesh Mamtani <73697822+dipesh04@users.noreply.github.com>
2021-11-23 17:14:32 +0530

    Update face_recognition.py

--- face_recognition.py
+++ face_recognition.py
@@ -49,7 +49,7 @@ cap = cv2.VideoCapture(0)
 face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_frontalface_alt.xml")
 
 skip = 0
-dataset_path = '/Users/HP/Desktop/CodingClub/Face_Recognition/data/'
+dataset_path = './data/'
 
 face_data = [] 
 labels = []
